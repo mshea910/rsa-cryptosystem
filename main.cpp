f227887dc021505b593b600a04ffdd21e646537e
Michael Shea <mshea910@gmail.com>
2023-04-10 11:44:54 -0400

    Set up initial welcome and variables

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Michael Shea on 4/10/23.
+#include "CryptoSystem.h"
+
+int main(void) {
+    int p;
+    int q;
+
+    std::cout << "\t~~ Welcome to the RSA CryptoSystem v1.0.0 ~~" << std::endl;
+    std::cout << "\nPlease enter your first Prime Number: " << std::endl;
+    std::cin >> p;
+    // Need to add Prime Number checking
+    std::cout << std::endl << "Please enter your second Prime Number: " << std::endl;
+    std::cin >> q;
+    // Need to add Prime Number checking
+}
+//
